4596cabfda71fa86475fa55d3e21feffe8820af8
Arthur Duboks <duboks.arthur@gmail.com>
2024-05-07 10:01:46 -0400

    reverse middleware

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,13 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isTranslateRoute = createRouteMatcher(["/translate(.*)"]);
-const isAdminRoute = createRouteMatcher(["/admin(.*)"]);
+// const isDashboardRoute = createRouteMatcher(["/dashboard(.*)"]);
+// const isAdminRoute = createRouteMatcher(["/admin(.*)"]);
 
 export default clerkMiddleware((auth, req) => {
   // Restrict admin route to users with specific role
-  if (isAdminRoute(req)) {
-    auth().protect({ role: "org:admin" });
-  }
-  // Restrict dashboard routes to signed in users
-  if (isTranslateRoute(req)) {
-    auth().protect();
-  }
+  //   if (isAdminRoute(req)) auth().protect({ role: "org:admin" });
+  // Restrict dashboard routes to logged in users
+  //   if (isDashboardRoute(req)) auth().protect();
 });
 
 export const config = {
