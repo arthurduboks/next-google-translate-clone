4f6a425419b6d9617c473e456066697a42253a32
Arthur Duboks <duboks.arthur@gmail.com>
2024-05-06 21:34:54 -0400

    update middleware

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,17 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-// const isTranslateRoute = createRouteMatcher(["/translate(.*)"]);
-// const isAdminRoute = createRouteMatcher(["/admin(.*)"]);
+const isTranslateRoute = createRouteMatcher(["/translate(.*)"]);
+const isAdminRoute = createRouteMatcher(["/admin(.*)"]);
 
 export default clerkMiddleware((auth, req) => {
-  // // Restrict admin route to users with specific role
-  // if (isAdminRoute(req)) auth().protect({ role: "org:admin" });
-  // // Restrict dashboard routes to signed in users
-  // if (isTranslateRoute(req)) auth().protect();
+  // Restrict admin route to users with specific role
+  if (isAdminRoute(req)) {
+    auth().protect({ role: "org:admin" });
+  }
+  // Restrict dashboard routes to signed in users
+  if (isTranslateRoute(req)) {
+    auth().protect();
+  }
 });
 
 export const config = {
