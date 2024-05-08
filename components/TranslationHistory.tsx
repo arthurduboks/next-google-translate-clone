ee71c4e4b5ff03973dbca1ca0140f9e5b65807c2
Arthur Duboks <duboks.arthur@gmail.com>
2024-05-08 17:10:14 -0400

    history fetch fix

--- components/TranslationHistory.tsx
+++ components/TranslationHistory.tsx
@@ -1,7 +1,7 @@
 import { ITranslation } from "@/mongodb/models/User";
 import { auth } from "@clerk/nextjs/server";
-import TimeAgoText from "./TimeAgoText";
 import DeleteTranslationButton from "./DeleteTranslationButton";
+import TimeAgoText from "./TimeAgoText";
 
 const getLanguage = (code: string) => {
   const lang = new Intl.DisplayNames(["en"], { type: "language" });
@@ -10,69 +10,59 @@ const getLanguage = (code: string) => {
 
 async function TranslationHistory() {
   const { userId } = auth();
-  const baseUrl =
+
+  const url = `${
     process.env.NODE_ENV === "development"
       ? "http://localhost:3000"
-      : `https://${process.env.VERCEL_URL}`;
+      : process.env.VERCEL_URL
+  }/translationHistory?userId=${userId}`;
 
-  const url = `${baseUrl}/translationHistory?userId=${userId}`;
+  const response = await fetch(url, {
+    next: {
+      tags: ["translationHistory"],
+    },
+  });
 
-  try {
-    const res = await fetch(url, {
-      next: {
-        tags: ["translationHistory"],
-      },
-    });
+  const { translations }: { translations: Array<ITranslation> } =
+    await response.json();
 
-    if (!res.ok) {
-      const errorBody = await res.text();
-      throw new Error(
-        `Failed to fetch translation history: ${res.status} - ${errorBody}`
-      );
-    }
-
-    const { translations }: { translations: Array<ITranslation> } =
-      await res.json();
-    return renderHistory(translations);
-  } catch (error) {
-    console.error("Error fetching translation history:", error);
-    return <p>Error loading translation history. Please try again later.</p>;
-  }
-}
-
-function renderHistory(translations: ITranslation[]) {
   return (
-    <div>
+    <div className="">
       <h1 className="text-3xl my-5">History</h1>
-      {translations.length === 0 ? (
-        <p className="mb-5 text-gray-400">No translations yet</p>
-      ) : (
-        <ul className="divide-y border rounded-md">
-          {translations.map((translation) => (
-            <li
-              key={translation._id}
-              className="flex justify-between items-center p-5 hover:bg-gray-50 relative"
-            >
-              <div>
-                <p className="text-sm mb-5 text-gray-500">
-                  {getLanguage(translation.from)} {"->"}{" "}
-                  {getLanguage(translation.to)}
-                </p>
-                <div className="space-y-2 pr-5">
-                  <p>{translation.fromText}</p>
-                  <p className="text-gray-400">{translation.toText}</p>
-                </div>
-              </div>
-              <p className="text-sm text-gray-300 absolute top-2 right-2">
-                <TimeAgoText
-                  date={new Date(translation.timestamp).toISOString()}
-                />
-              </p>
-              <DeleteTranslationButton id={translation._id} />
-            </li>
-          ))}
-        </ul>
+
+      {translations.length === 0 && (
+        <p className="mb-5 text-gray-400">No translations yet.</p>
       )}
+
+      <ul className="divide-y border rounded-md">
+        {translations.map((translation) => (
+          <li
+            key={translation._id}
+            className="flex justify-between items-center p-5 hover:bg-gray-50 relative"
+          >
+            <div>
+              <p className="text-sm mb-5 text-gray-500">
+                {getLanguage(translation.from)}
+                {" -> "}
+                {getLanguage(translation.to)}
+              </p>
+
+              <div className="space-y-2 pr-5">
+                <p>{translation.fromText}</p>
+                <p className="text-gray-400">{translation.toText}</p>
+              </div>
+            </div>
+
+            <p className="text-sm text-gray-300 absolute top-2 right-2">
+              <TimeAgoText
+                date={new Date(translation.timestamp).toISOString()}
+              />
+            </p>
+
+            <DeleteTranslationButton id={translation._id} />
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
